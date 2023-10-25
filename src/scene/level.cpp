408e81c4285aa32bd9d336d068db3b90ca450812
VitorTz <vitor.fsz@proton.me>
2023-10-25 19:23:18 -0300

    Update

--- src/scene/level.cpp
+++ src/scene/level.cpp
@@ -4,8 +4,5 @@
 og::Level::Level(
     std::function<void(const og::SceneId&)> changeScene
 ) : og::Scene(og::SceneId::Level, changeScene) {
-    og::GameObj* player = new og::GameObj("Player");
-    player->group.add(new og::Sprite("PlayerImage", og::ImageId::Icon), player);
-    this->gameObjs.add(player);
-    this->camera.submit(player);
+    
 }
